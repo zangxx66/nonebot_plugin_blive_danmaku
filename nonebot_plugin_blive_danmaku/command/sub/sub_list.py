4c0a034828ebcf489252d92b1a888380c6994142
ricardo <thespirit@vip.qq.com>
2023-04-12 11:25:50 +0800

    去掉无用代码

--- nonebot_plugin_blive_danmaku/command/sub/sub_list.py
+++ nonebot_plugin_blive_danmaku/command/sub/sub_list.py
@@ -1,9 +1,10 @@
 from nonebot.adapters.onebot.v11.event import MessageEvent
 from nonebot.adapters.onebot.v11 import Bot
-from nonebot import on_command
+from nonebot import on_command, get_driver
 from ...utils import get_type_id,permission_check
 from bilireq.user import get_user_info
 from ...database import Db as db
+from ...config import Config
 
 
 sub_list = on_command("订阅列表", priority=5)
@@ -16,11 +17,13 @@ async def _(event: MessageEvent, bot: Bot):
     type_id = await get_type_id(event)
     subs = await db.get_subs(type=event.message_type, type_id=type_id)
     msg = "订阅列表：\n\n"
+    plugin_config = Config.parse_obj(get_driver().config)
     for sub in subs:
         user = await get_user_info(sub.uid, reqtype="web", proxies=None)
         name = user["name"]
         msg += (
-            f"{name}({sub.uid})"
-            f"路灯：{'开' if sub.street_lamp else '关'}"
+            f"{name}({sub.uid}) "
+            f"路灯：{'开' if sub.street_lamp else '关'} "
+            f"开播提醒：{'开' if plugin_config.danmaku_group_notice else '关'}"
         )
     await sub_list.finish(msg)
