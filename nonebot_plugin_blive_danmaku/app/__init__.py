688a0820edf2401a94af33135790e9bcaaed2480
ricardo <thespirit@vip.qq.com>
2023-04-12 08:20:35 +0800

    添加跨域

--- nonebot_plugin_blive_danmaku/app/__init__.py
+++ nonebot_plugin_blive_danmaku/app/__init__.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from .router import router
+from nonebot.log import logger
 
 
 dist_path = Path(__file__).parent / "frontend"
@@ -11,6 +15,14 @@ if not dist_path.is_dir():
     raise FileNotFoundError("WebUI path not found")
 
 app = FastAPI(title="nonebot_plugin_blive_danmaku", description="live room danmaku manager", dependencies=([]), version="0.2.0")
+
+@app.exception_handler(RequestValidationError)
+async def exception_handle(request: Request, exc: RequestValidationError):
+    logger.error(f"fastapi请求异常：{exc.errors()}")
+    return JSONResponse({"code": -1, "msg": exc.errors(), "data": None})
+
+
 app.add_middleware(GZipMiddleware, minimum_size=1024)
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
 app.include_router(router, prefix="/api")
 app.mount("/", StaticFiles(directory=dist_path, html=True), name="frontend")
--- nonebot_plugin_blive_danmaku/app/router.py
+++ nonebot_plugin_blive_danmaku/app/router.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from ..database import Db as db
 from . import models
 
 router = APIRouter(tags=["api"])
 
-@router.get("/list/{type}/{type_id}", response_model=models.ResponseItem)
-async def get_type_sub_list(type:str, type_id: int):
+@router.get("/get_sub_list", response_model=models.ResponseItem)
+async def get_type_sub_list(type:str = Query(..., max_length=50), type_id: int = Query(...), page:int = Query(1), size:int = Query(30)):
     sub_list = await db.get_subs(type=type, type_id=type_id)
     room_list = []
     for sub in sub_list:
@@ -13,12 +13,16 @@ async def get_type_sub_list(type:str, type_id: int):
         if not room:
             continue
         room_list.append(room)
-    return models.ResponseItem(code=0, msg="", data={"rows": room_list})
+    total = len(room_list)
+    start = (page - 1) * size
+    end = page * size
+    result = room_list[start:end]
+    return models.ResponseItem(code=0, msg="", data={"rows": result, "total": total})
 
-@router.get("/room/{room_id}", response_model=models.ResponseItem)
-async def get_room_info(room_id: int):
+@router.get("/get_room", response_model=models.ResponseItem)
+async def get_room_info(room_id: int = Query(...)):
     room_info = await db.get_room(room_id=room_id)
     if not room_info:
         return models.ResponseItem(code=-1, msg="房间号码有误", data=None)
     danmaku_list = await db.get_danmaku_by_rid(room_id=room_id)
-    return models.ResponseItem(code=0, msg="", data={"room_indo": room_info, "danmaku": danmaku_list})
+    return models.ResponseItem(code=0, msg="", data={"room_info": room_info, "danmaku": danmaku_list})
