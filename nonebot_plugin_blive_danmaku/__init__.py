a7409234a92feb0fd4527f42a94bf2be8fc0c03e
ricardo <thespirit@vip.qq.com>
2023-06-24 14:22:50 +0800

    适配nonebot2 2.0.0

--- nonebot_plugin_blive_danmaku/__init__.py
+++ nonebot_plugin_blive_danmaku/__init__.py
@@ -8,10 +8,13 @@ __plugin_meta__ = PluginMetadata(
     name = "B站直播间路灯插件",
     description = "通过弹幕记录直播高能点",
     usage = "弹幕指令为'#路灯 加上记录的内容'，仅在开播时弹幕指令才会生效",
+    type = "application",
+    homepage = "https://github.com/zangxx66/nonebot_plugin_blive_danmaku",
     config = danmaku_config,
+    supported_adapters = {"~onebot.v11"},
     extra = {}
 )
-__version__ = "0.2.7"
+__version__ = "0.3.0"
 
 driver = get_driver()
 driver.on_startup(on_startup)
